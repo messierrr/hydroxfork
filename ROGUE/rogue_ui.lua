e92d704846acb8649d5ad07b99b1b1fca66346ae
messierrr <317292490+messierrr@users.noreply.github.com>
2026-09-21 19:53:56 -0500

    Update rogue_ui.lua

--- ROGUE/rogue_ui.lua
+++ ROGUE/rogue_ui.lua
@@ -7408,7 +7408,7 @@ if game.PlaceId == 3541987450 or game.PlaceId == 5208655184 or game.PlaceId == 1
         local Toggles = library.Toggles
 
         local window = library:CreateWindow({
-            Title = HXD_UserNote and string.format("Hydroxide | %s", HXD_UserNote:sub(1,1):upper() .. HXD_UserNote:sub(2)) or "Hydroxide",
+            Title = "VEXWARE",
             NotifySide = "Left",
             Footer = "",
             Center = true,
